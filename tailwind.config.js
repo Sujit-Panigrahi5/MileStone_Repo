7ad7af50c91d36d08f487d2ae5611044ca7caf21
Sujit-Panigrahi5 <panigrahisujit588@gmail.com>
2023-07-24 23:32:14 +0530

    add some code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./project3/dist/*.html","./project4/dist"],
+  content: ["./project3/dist/*.html","./project4/dist/*.html","./project5/dist/*.html"],
   theme: {
     extend: {},
   },
